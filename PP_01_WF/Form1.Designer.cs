faa18ffd75762e6a7cd3676bd647dfb12a46c1ca
vitalekk1 <vitalikkakanov55@gmail.com>
2020-04-13 11:45:43 +0500

    Добавьте файлы проекта.

---
+++ PP_01_WF/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PP_01_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Result_Integral = new System.Windows.Forms.Button();
+            this.LB_Result_Integral = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn_Result_Integral
+            // 
+            this.Btn_Result_Integral.Location = new System.Drawing.Point(302, 7);
+            this.Btn_Result_Integral.Name = "Btn_Result_Integral";
+            this.Btn_Result_Integral.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Result_Integral.TabIndex = 0;
+            this.Btn_Result_Integral.Text = "Посчитать";
+            this.Btn_Result_Integral.UseVisualStyleBackColor = true;
+            this.Btn_Result_Integral.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LB_Result_Integral
+            // 
+            this.LB_Result_Integral.AutoSize = true;
+            this.LB_Result_Integral.Location = new System.Drawing.Point(137, 12);
+            this.LB_Result_Integral.Name = "LB_Result_Integral";
+            this.LB_Result_Integral.Size = new System.Drawing.Size(0, 13);
+            this.LB_Result_Integral.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Значение интегралла:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LB_Result_Integral);
+            this.Controls.Add(this.Btn_Result_Integral);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Result_Integral;
+        private System.Windows.Forms.Label LB_Result_Integral;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PP_01_WF/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PP_01_WF
+{
+    public partial class Form1 : Form
+    {
+        private delegate void SafeCallDelegate(string text);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static double h1;
+        public double[] results;
+        private struct Struct1
+        {
+            public double start;
+            public double end;
+            public int countN;
+            public int countThread;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double start, end;
+            int countN;
+            start = 4;
+            end = 10;
+            countN = 3;
+            results = new double[countN];
+            Struct1 struct1 = new Struct1();
+            struct1.start = start;
+            struct1.end = end;
+            struct1.countN = countN;
+            struct1.countThread = 0;
+            Thread thread = new Thread(devide);
+            thread.Start(struct1);
+        }
+
+        private void devide(object struct1)
+        {
+            Struct1 struct2 = (Struct1)struct1;
+            double start = struct2.start;
+            double end = struct2.end;
+            int countN = struct2.countN;
+            devideCountN(start, end, countN);
+            int countThread = 0;
+            double endT = start + h1;
+            Thread[] threads = new Thread[countN];
+
+            for (double i = start; i < end; i += h1)
+            {
+                Struct1 struct3 = new Struct1();
+                lock ((object)results)
+                {
+                    endT = i + h1;
+                    if (endT > end) { endT = end; }
+                    struct3.start = i;
+                    struct3.end = endT;
+                    struct3.countN = 10000;
+                    struct3.countThread = countThread;
+                    threads[countThread] = new Thread(calculator);
+                    threads[countThread].Start(struct3);
+                    countThread++;
+                }
+            }
+
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+
+            double resultsAll = 0;
+            
+            for (int i = 0; i < countThread; i++) {
+                resultsAll += results[i];
+            }
+
+            WriteTextSafe(resultsAll.ToString());
+
+        }
+
+        private void WriteTextSafe(string text)
+        {
+            if (LB_Result_Integral.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteTextSafe);
+                LB_Result_Integral.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                LB_Result_Integral.Text = text;
+            }
+        }
+
+        private  void devideCountN(double start, double end, int countN)
+        {
+            h1 = Math.Round((end - start) / countN);
+        }
+
+        private  void calculator(object struct1)
+        {
+            double result = 0;
+            Struct1 struct2 = (Struct1)struct1;
+            double start = struct2.start;
+            double end = struct2.end;
+            int countN = struct2.countN;
+            int countThread = struct2.countThread;
+            results[countThread] = threadCalc(start, end, countN);
+        }
+
+        private double f(double x)
+        {
+            return Math.Pow(2, x) / Math.Pow(x, 3);
+        }
+
+        private double threadCalc(double start, double end, double countN)
+        {
+            double result = 0;
+            double h = (end - start) / countN;
+            int i;
+            for (i = 0; i < countN; i++)
+            {
+                result += f(start + h * (i + 0.5));
+            }
+            result *= h;
+            return result;
+        }
+
+    }
+}
